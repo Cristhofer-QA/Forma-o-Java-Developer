4560a725f3cfde0c6032768a6ae3922c86dacd76
Cristhofer Mian <cristhofer.mian@e-stracta.com>
2024-04-18 20:14:53 -0300

    Inclusão de pacotes

--- anatomia-classes-java/src/edu/cristhofer/primeirasemana/MinhaClasse.java
+++ anatomia-classes-java/src/edu/cristhofer/primeirasemana/MinhaClasse.java
@@ -1,14 +1,16 @@
+package edu.cristhofer.primeirasemana;
+
 public class MinhaClasse {
     public static void main(String[] args) {
         String primeiroNome = "Cristhofer";
         String segundoNome = "Mian";
-        
+
         String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
 
         System.out.println(nomeCompleto);
     }
 
-    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
         return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
